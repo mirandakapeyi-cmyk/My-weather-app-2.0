7ae437b5dbd079563e17ffc0372e358628c9fe22
mirandakapeyi-cmyk <mirandakapeyi@gmail.com>
2026-02-23 21:02:09 +0200

    added search functionality using the Api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-function handleseachersubmit(event){
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function seacherCity (city) {
+let apiKey = "e34395o3e105b4b61f3t4af72402c233";
+let apiUrl =
+  `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSeacherSubmit(event){
     event.preventDefault();
     let searchInputElement = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInputElement.value;
+   
+    seacherCity(searchInputElement.value);
 }
 
 let seacherFormElement = document.querySelector("#search-form");
- seacherFormElement.addEventListener("submit", handleseachersubmit);
+ seacherFormElement.addEventListener("submit", handleSeacherSubmit);
+
+ seacherCity("Cape Town");
