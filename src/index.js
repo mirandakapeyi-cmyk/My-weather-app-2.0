0a87454d53d1e5c9d77ed6051c53bc40f15bb442
mirandakapeyi-cmyk <mirandakapeyi@gmail.com>
2026-02-23 21:05:27 +0200

    corrected spelling

--- src/index.js
+++ src/index.js
@@ -6,21 +6,21 @@ function refreshWeather(response) {
   cityElement.innerHTML = response.data.city
   temperatureElement.innerHTML = Math.round(temperature);
 }
-function seacherCity (city) {
+function searchCity (city) {
 let apiKey = "e34395o3e105b4b61f3t4af72402c233";
 let apiUrl =
   `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
-function handleSeacherSubmit(event){
+function handleSearchSubmit(event){
     event.preventDefault();
     let searchInputElement = document.querySelector("#search-form-input");
    
-    seacherCity(searchInputElement.value);
+    searchCity(searchInputElement.value);
 }
 
 let seacherFormElement = document.querySelector("#search-form");
- seacherFormElement.addEventListener("submit", handleSeacherSubmit);
+ seacherFormElement.addEventListener("submit", handleSearchSubmit);
 
- seacherCity("Cape Town");
+ searchCity("Cape Town");
